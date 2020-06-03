6398de2530a92fd57be3d2ea0be19a62f31eefa2
Somya-5353 <somyacarmel5353@gmail.com>
2020-06-03 22:54:09 +0530

    added travel data model

---
+++ Models/travel_data.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'travel_model.dart';
+
+const TRAVEL_DATA = const [
+  TravelModel(
+    id: 't1',
+    title: 'Beaches',
+    color: Colors.lightBlue,
+  ),
+  TravelModel(
+    id: 't2',
+    title: 'Safaris',
+    color: Colors.green,
+  ),
+  TravelModel(
+    id: 't3',
+    title: 'Hill Stations',
+    color: Colors.brown,
+  ),
+  TravelModel(
+    id: 't4',
+    title: 'Safaris',
+    color: Colors.green,
+  ),
+  TravelModel(
+    id: 't5',
+    title: 'Backpacking',
+    color: Colors.red,
+  ),
+  TravelModel(
+    id: 't6',
+    title: 'Trekking',
+    color: Colors.yellowAccent,
+  ),
+];
